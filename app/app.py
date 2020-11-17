bc822672a71a4d83832ec0015f8c0976c2eafda5
Brad Traversy <ajpDrumm@gmail.com>
2020-11-17 18:13:07 -0500

    Task: edited app.py. Added index.html & layout.html

--- app/app.py
+++ app/app.py
@@ -27,7 +27,13 @@ def cities_import() -> List[Dict]:
 
 
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username' : 'Miguel')
+    cities_data = cities_import()}
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
