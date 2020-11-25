403ad41a7e323aab20db50e12551395a605ff33c
Brad Traversy <ajpDrumm@gmail.com>
2020-11-24 19:04:36 -0500

    Task: copied over assignment 3 code

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 
 app = Flask(__name__)
-user = {'username': 'Wenbo Wang'}
+user = {'username': 'Andrew Drumm'}
 
 class MyDb:
     def __init__(self):
