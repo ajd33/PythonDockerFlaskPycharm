8cb54beaaa9920c2a1efb18e769b8e18ad52c1ca
Brad Traversy <ajpDrumm@gmail.com>
2020-12-01 13:02:04 -0500

    bugfix: fixed app.py init.sql edit and index

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ class MyDb:
             'password': 'root',
             'host': 'db',
             'port': '3306',
-            'database': 'FordEscortData'
+            'database': 'fordEscort'
         }
         self.connection = mysql.connector.connect(**config)
 
@@ -23,31 +23,31 @@ class MyDb:
 
     def get_alldata(self):
         cursor = self.connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM ford_escort')
+        cursor.execute('SELECT * FROM escort')
         return cursor.fetchall()
 
-    def get_rating(self, rating_id):
+    def get_mileage(self, mileage_id)_:
         cursor = self.connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM ford_escort WHERE id=%s', (rating_id,))
+        cursor.execute('SELECT * FROM escort WHERE id=%s', (mileage_id,))
         result = cursor.fetchall()
         return result[0]
 
-    def update_rating(self, inputData):
+    def update_mileage(self, inputData):
         cursor = self.connection.cursor(dictionary=True)
-        sql_update_query = """UPDATE ford_escort t SET t.Year = %s, t.Score = %s, t.Title = %s WHERE t.id = %s """
+        sql_update_query = """UPDATE escort t SET t.Year = %s, t.Mileage = %s, t.Price = %s WHERE t.id = %s """
         cursor.execute(sql_update_query, inputData)
         self.connection.commit()
 
-    def insert_rating(self, inputData):
+    def insert_mileage(self, inputData):
         cursor = self.connection.cursor(dictionary=True)
-        sql_insert_query = """INSERT INTO ford_escort (`Year`,Score,Title) VALUES (%s, %s, %s) """
+        sql_insert_query = """INSERT INTO escort (`Year`,Mileage,Price) VALUES (%s, %s, %s) """
         cursor.execute(sql_insert_query, inputData)
         self.connection.commit()
 
-    def delete_rating(self, rating_id):
+    def delete_mileage(self, mileage_id):
         cursor = self.connection.cursor(dictionary=True)
-        sql_delete_query = """DELETE FROM ford_escort WHERE id = %s """
-        cursor.execute(sql_delete_query, (rating_id,))
+        sql_delete_query = """DELETE FROM escort WHERE id = %s """
+        cursor.execute(sql_delete_query, (mileage_id,))
         self.connection.commit()
 
 
@@ -56,78 +56,77 @@ db = MyDb()
 
 @app.route('/')
 def index():
-    deniro = db.get_alldata()
-    return render_template('index.html', title='Home', user=user, deniro=deniro)
+    escort = db.get_alldata()
+    return render_template('index.html', Price='Home', user=user, escort=escort)
 
-@app.route('/view/<int:rating_id>', methods=['GET'])
-def record_view(rating_id):
-    rating = db.get_rating(rating_id)
-    return render_template('view.html', title='View Form', user=user, rating=rating)
-
-
-@app.route('/edit/<int:rating_id>', methods=['GET'])
-def form_edit_get(rating_id):
-    rating = db.get_rating(rating_id)
-    return render_template('edit.html', title='Edit Form', user=user, rating=rating)
+@app.route('/view/<int:mileage_id>', methods=['GET'])
+def record_view(mileage_id):
+    mileage = db.get_mileage(mileage_id)
+    return render_template('view.html', Price='View Form', user=user, mileage=mileage)
 
 
-@app.route('/edit/<int:rating_id>', methods=['POST'])
-def form_update_post(rating_id):
-    inputData = (request.form.get('Year'), request.form.get('Score'), request.form.get('Title'), rating_id)
-    db.update_rating(inputData)
+@app.route('/edit/<int:mileage_id>', methods=['GET'])
+def form_edit_get(mileage)_id):
+    mileage = db.get_mileage(mileage_id)
+    return render_template('edit.html', Price='Edit Form', user=user, mileage=mileage)
+
+
+@app.route('/edit/<int:mileage_id>', methods=['POST'])
+def form_update_post(mileage_id):
+    inputData = (request.form.get('Year'), request.form.get('Mileage'), request.form.get('Price'), mileage_id)
+    db.update_mileage(inputData)
     return redirect("/", code=302)
 
-@app.route('/rating/new', methods=['GET'])
+@app.route('/mileage/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New Rating Form', user=user)
+    return render_template('new.html', Price='New Mileage Form', user=user)
 
 
-@app.route('/rating/new', methods=['POST'])
+@app.route('/mileage/new', methods=['POST'])
 def form_insert_post():
-    inputData = (request.form.get('Year'), request.form.get('Score'), request.form.get('Title'))
-    db.insert_rating(inputData)
+    inputData = (request.form.get('Year'), request.form.get('Mileage'), request.form.get('Price'))
+    db.insert_mileage(inputData)
     return redirect("/", code=302)
 
-@app.route('/delete/<int:rating_id>', methods=['POST'])
-def form_delete_post(rating_id):
-    db.delete_rating(rating_id)
+@app.route('/delete/<int:mileage_id>', methods=['POST'])
+def form_delete_post(mileage_id):
+    db.delete_mileage(mileage_id)
     return redirect("/", code=302)
 
+# API version 1
 
-# API v1
-
-@app.route('/api/v1/ratings')
-def api_ratings() -> str:
+@app.route('/api/v1/mileage')
+def api_mileage() -> str:
     js = json.dumps(db.get_alldata())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
-@app.route('/api/v1/ratings/<int:rating_id>', methods=['GET'])
-def api_retrieve(rating_id) -> str:
-    result = db.get_rating(rating_id)
+@app.route('/api/v1/mileages/<int:mileage_id>', methods=['GET'])
+def api_retrieve(mileage_id) -> str:
+    result = db.get_mileage(mileage_id)
     json_result = json.dumps(result)
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
-@app.route('/api/v1/ratings/', methods=['POST'])
+@app.route('/api/v1/mileages/', methods=['POST'])
 def api_add() -> str:
-    inputData = (request.form.get('Year'), request.form.get('Score'), request.form.get('Title'))
-    db.insert_rating(inputData)
+    inputData = (request.form.get('Year'), request.form.get('Mileage'), request.form.get('Price'))
+    db.insert_mileage(inputData)
     resp = Response(status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/ratings/<int:rating_id>', methods=['PUT'])
-def api_edit(rating_id) -> str:
-    inputData = (request.form.get('Year'), request.form.get('Score'), request.form.get('Title'), rating_id)
-    db.update_rating(inputData)
+@app.route('/api/v1/mileages/<int:mileage_id>', methods=['PUT'])
+def api_edit(mileage_id) -> str:
+    inputData = (request.form.get('Year'), request.form.get('Mileage'), request.form.get('Price'), mileage_id)
+    db.update_mileage(inputData)
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/ratings/<int:rating_id>', methods=['DELETE'])
-def api_delete(rating_id) -> str:
-    db.delete_rating(rating_id)
+@app.route('/api/mileages/<int:mileage_id>', methods=['DELETE'])
+def api_delete(mileage_id) -> str:
+    db.delete_mileage(mileage_id)
     resp = Response(status=210, mimetype='application/json')
     return resp
 
